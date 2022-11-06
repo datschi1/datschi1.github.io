da0da419a4f79587ec299d9d91ce5a287c9ceab7
Dachi <datschigordiashvili@gmail.com>
2022-11-06 14:39:49 +0400

    done

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function moveBackground(event){
     for (let i = 0; i<shapes.length;i++){
         const isOdd = i%2 !== 0;
         const oddInteger = isOdd ? -1 : 1
-        shapes[i].style.transform = `translate(${x*oddInteger}px , ${y*oddInteger})`
+        shapes[i].style.transform = `translate(${x*oddInteger}px , ${y*oddInteger}px)`
     }
 }
 
